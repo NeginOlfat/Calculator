fc518b91b9de7019b26db92ecea59193a5948573
NeginOlfat <neginolfat@yahoo.com>
2022-01-02 21:45:45 +0330

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View,Text} from "react-native";
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <View>
+
+      </View>
+    )
+  }
+}
+
+export default App;
